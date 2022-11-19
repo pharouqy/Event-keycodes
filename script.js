7b50d9f2c16e25d1c072ca30ad569fd6be5799db
farouky <farouky@gmail.com>
2022-11-19 14:34:54 +0100

    Finalisation Of Project !!!

---
+++ script.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector("h1");
+const container = document.getElementById("container");
+const page = document.getElementsByTagName("body");
+
+console.log(page);
+
+page[0].addEventListener("keydown", (e) => {
+  e.preventDefault();
+  btn.style.display = "none";
+  for (let i = 0; i < 3; i++) {
+    let card = document.createElement("div");
+    let h2 = document.createElement("h2");
+    card.appendChild(h2);
+    container.appendChild(card);
+  }
+
+  const info = document.getElementsByTagName("h2");
+  info[0].textContent = e.key;
+  info[1].textContent = e.keyCode;
+  info[2].textContent = e.code;
+  const cards = document.getElementsByTagName("div");
+  for (let i = 4; i < cards.length; i++) {
+    cards[i].style.display = "none";
+  }
+});
